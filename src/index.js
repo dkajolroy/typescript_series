af0363676996246590e108ed10655f02ac47952c
kajolroy <shrikajol@gmail.com>
2023-01-16 02:56:26 +0600

    Generic

--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
 "use strict";
-// OOP
 exports.__esModule = true;
-//OOP System 1
-var User = /** @class */ (function () {
-    function User(name, role) {
-        this.name = name;
-        this.role = role;
-    }
-    User.prototype.log = function () {
-        console.log(this.name, this.role);
-    };
-    return User;
-}());
-var kajol = new User("Kajol", 22);
-kajol.log();
-//OOP System 2
-var Student = /** @class */ (function () {
-    function Student(name, address) {
-        this.name = name;
-        this.address = address;
-    }
-    Student.prototype.log = function () {
-        console.log(this.name, this.address);
-    };
-    return Student;
-}());
-var student1 = new Student("Kajol 1", "Lal");
-console.log(student1.log());
+// Generic
+var addExam = function (x) {
+    return x;
+};
+var exam = {
+    no: 1,
+    sub: "Bangla"
+};
+console.log(addExam(exam));
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,13 @@
-// OOP
+// Generic
+const addExam = <T extends { no: number; sub: string }>(x: T): T => {
+  return x;
+};
 
-//OOP System 1
-class User {
-  name: string;
-  role: number;
-  constructor(name: string, role: number) {
-    this.name = name;
-    this.role = role;
-  }
+const exam = {
+  no: 1,
+  sub: "Bangla",
+};
 
-  log() {
-    console.log(this.name, this.role);
-  }
-}
-const kajol = new User("Kajol", 22);
-kajol.log();
-
-//OOP System 2
-class Student {
-  constructor(public name: string, private address: string) {}
-  log() {
-    console.log(this.name, this.address);
-  }
-}
-const student1 = new Student("Kajol 1", "Lal");
-console.log(student1.log());
+console.log(addExam(exam));
 
 export {};
