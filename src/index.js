04afb7fdfce6226ab21cf7c772a66b8ae7846c65
kajolroy <shrikajol@gmail.com>
2023-01-16 01:14:52 +0600

    Data types

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("Kam Hoiche ");
+"use strict";
+// Data types
+// String, Number, Boolean, Undefined, Null
+exports.__esModule = true;
+//Stings types
+var name;
+name = "ami";
+console.log(name);
+//Number types
+var age;
+age = 22;
+console.log(age);
+//Arrays
+var user;
+user = ["kajol", "Ray"];
+console.log(user);
+// Array of arrays
+var sub;
+sub = [["kajol", "Ray"]];
+console.log(sub);
+var userInfo;
+userInfo = {
+    name: "Kajol Roy",
+    age: 22,
+    married: false
+};
+console.log(userInfo);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log("Kam Hoiche ");
+// Data types
+// String, Number, Boolean, Undefined, Null
+
+//Stings types
+let name: string;
+name = "ami";
+console.log(name);
+
+//Number types
+let age: number;
+age = 22;
+console.log(age);
+
+//Arrays
+let user: string[];
+user = ["kajol", "Ray"];
+console.log(user);
+
+// Array of arrays
+let sub: string[][];
+sub = [["kajol", "Ray"]];
+console.log(sub);
+
+//Object Types
+type UserInterface = {
+  name: string;
+  age: number;
+  married: boolean;
+  address?: string;
+};
+let userInfo: UserInterface;
+userInfo = {
+  name: "Kajol Roy",
+  age: 22,
+  married: false,
+};
+console.log(userInfo);
+
+export {};
